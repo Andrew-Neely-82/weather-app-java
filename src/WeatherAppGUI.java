b995867b9f906336348584a5f4c610b50b243a3a
Andrew-Neely-82 <andrew.neely82@gmail.com>
2024-03-17 17:01:37 -0500

    added search field and button components to GUI

--- src/WeatherAppGUI.java
+++ src/WeatherAppGUI.java
@@ -1,4 +1,9 @@
+import javax.imageio.*;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
 
 public class WeatherAppGUI extends JFrame {
     public WeatherAppGUI() {
@@ -11,5 +16,30 @@ public class WeatherAppGUI extends JFrame {
         setLocationRelativeTo(null); // load gui to center of screen
         setLayout(null); // allows for manual positioning of components in GUI
         setResizable(false); // disables window resizing
+        addGuiComponents();
+    }
+
+    private void addGuiComponents() {
+        JTextField searchTextField = new JTextField();
+        searchTextField.setBounds(15, 15, 351, 45);
+        searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+        add(searchTextField);
+
+        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375, 13, 47, 45);
+        add(searchButton);
+    }
+
+    private ImageIcon loadImage(String path) {
+        try {
+            BufferedImage image = ImageIO.read(new File(path));
+            return new ImageIcon(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Could not find resource");
+        return null;
     }
 }
